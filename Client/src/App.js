a56303c947e972822cae79ea928e0359c07c09d0
Quach Cong Tuan <tuanqcbhaf200014@fpt.edu.vn>
2022-11-14 22:45:06 +0700

    Create Server and Client

--- Client/src/App.js
+++ Client/src/App.js
@@ -12,6 +12,7 @@ function App() {
     <div className="App">
         <Router>
           <Routes>
+            <Route path="/" element={<Home/>} />
             <Route path="/home" element={<Home/>} />
             <Route path="/messages" element={<Messages/>} />
             <Route path="/matches" element={<Matches/>} />
