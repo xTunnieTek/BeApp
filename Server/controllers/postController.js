75153559ccac921b32517b75bf772d8a381c3beb
Quach Cong Tuan <tuanqcbhaf200014@fpt.edu.vn>
2022-11-17 00:37:50 +0700

    Update postController.js
    Delete and Update function

--- Server/controllers/postController.js
+++ Server/controllers/postController.js
@@ -17,3 +17,24 @@ exports.getAll = async (req,res,next) => {
         posts
     })
 }
+
+// Delete Post
+exports.deletePost = async (req,res,next) => {
+    const {id} = req.body;
+    const post = await Post.findByIdAndDelete(id);
+    return res.status(200).json({
+        status:"success",
+        post
+    })
+}
+
+// Update Post
+exports.updatePost = async (req,res,next) => {
+    const {id,content} = req.body;
+    const post = await Post.findByIdAndUpdate(id , {content});
+    return res.status(200).json({
+        status:"success",
+        post
+    })
+}
+
