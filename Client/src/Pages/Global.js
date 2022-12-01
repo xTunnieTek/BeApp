2d221f35b5d595b58f2a278b8c0b51e3eca56689
Duc My Pham <ducmypham2610@gmail.com>
2022-12-01 14:59:45 +0700

    update Global

--- Client/src/Pages/Global.js
+++ Client/src/Pages/Global.js
@@ -220,39 +220,6 @@ function Global() {
           </div>
           ))}
           {/* End Post */}
-
-          {/* Start Post */}
-          <div className="Post">
-            <div className="Info">
-              <div className="Avatar">
-                <img src={Avatar} alt="Avatar" />
-              </div>
-              <div className="Name">
-                <h3>Anh Ngoc</h3>
-                <p>1 hour ago</p>
-              </div>
-            </div>
-            <div className="Content">
-              <p>What's on your mind ?</p>
-            </div>
-            <div className="Image">
-              <img src={Avatar} alt="Avatar" />
-            </div>
-            <div className="Action">
-              <div className="Like">
-                <button>
-                  <img src={Like} />
-                  Like
-                </button>
-                <button>
-                  <img src={Comment} />
-                  Comment
-                </button>
-              </div>
-            </div>
-          </div>
-
-          {/* End Post */}
         </div>
       </div>
     </MainLayout>
